61a4b2e4eb10f3559a8734ff7a9d6f49e3844fae
csx <18234834396@163.com>
2024-06-08 00:22:14 +0800

    优化NDK

--- efndk/src/main/cpp/napi_init.cpp
+++ efndk/src/main/cpp/napi_init.cpp
@@ -1,150 +1,141 @@
 #include "napi/native_api.h"
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 #include <hilog/log.h>
+#include <openssl/des.h>
+#include <string>
 
-/**
- * 生成deskey
- * @param env
- * @param info
- * @return
- */
-static napi_value encodeECB(napi_env env, napi_callback_info info) {
-    if ((nullptr == env) || (nullptr == info)) {
-        OH_LOG_Print(LOG_APP, LOG_ERROR, 0xFF00, "MyHypot", "env or exports is null");
-        return nullptr;
-    }
-
-    // Number of parameters.
+static napi_value Add(napi_env env, napi_callback_info info) {
     size_t argc = 2;
-
-    // Declare parameter array.
     napi_value args[2] = {nullptr};
 
-    // Gets the arguments passed in and puts them in the argument array.
-    if (napi_ok != napi_get_cb_info(env, info, &argc, args, nullptr, nullptr)) {
-        OH_LOG_Print(LOG_APP, LOG_ERROR, 0xFF00, "MyHypot", "api_get_cb_info failed");
-        return nullptr;
-    }
+    napi_get_cb_info(env, info, &argc, args, nullptr, nullptr);
 
-   
-    napi_value napiResult;
-   
-    return napiResult;
+    napi_valuetype valuetype0;
+    napi_typeof(env, args[0], &valuetype0);
+
+    napi_valuetype valuetype1;
+    napi_typeof(env, args[1], &valuetype1);
+
+    double value0;
+    napi_get_value_double(env, args[0], &value0);
+
+    double value1;
+    napi_get_value_double(env, args[1], &value1);
+
+    napi_value sum;
+    napi_create_double(env, value0 + value1, &sum);
+
+    return sum;
 }
 
-static napi_value decodeECB(napi_env env, napi_callback_info info) {
-    if ((nullptr == env) || (nullptr == info)) {
-        OH_LOG_Print(LOG_APP, LOG_ERROR, 0xFF00, "MyHypot", "env or exports is null");
-        return nullptr;
-    }
-    // Number of parameters.
-    size_t argc = 2;
-
-    // Declare parameter array.
-    napi_value args[2] = {nullptr};
-
-    // Gets the arguments passed in and puts them in the argument array.
-    if (napi_ok != napi_get_cb_info(env, info, &argc, args, nullptr, nullptr)) {
-        OH_LOG_Print(LOG_APP, LOG_ERROR, 0xFF00, "MyHypot", "api_get_cb_info failed");
-        return nullptr;
-    }
-
-
+static napi_value EncodeCBC(napi_env env, napi_callback_info info) {
+    // 返回加密结果
     napi_value napiResult;
-
-    return napiResult;
-}
-
-static napi_value encodeCBC(napi_env env, napi_callback_info info) {
-    if ((nullptr == env) || (nullptr == info)) {
-        OH_LOG_Print(LOG_APP, LOG_ERROR, 0xFF00, "MyHypot", "env or exports is null");
-        return nullptr;
-    }
-    // Number of parameters.
+    // 获取入参
     size_t argc = 2;
-
-    // Declare parameter array.
     napi_value args[2] = {nullptr};
-
-    // Gets the arguments passed in and puts them in the argument array.
-    if (napi_ok != napi_get_cb_info(env, info, &argc, args, nullptr, nullptr)) {
-        OH_LOG_Print(LOG_APP, LOG_ERROR, 0xFF00, "MyHypot", "api_get_cb_info failed");
-        return nullptr;
+    // 实际赋值
+    napi_get_cb_info(env, info, &argc, args, nullptr, nullptr);
+    // 获取des的key
+    char key[256];
+    size_t keyLen;
+    napi_status stKey = napi_get_value_string_utf8(env, args[0], key, sizeof(key), &keyLen);
+    if (stKey != napi_ok) {
+        char msg[] = "读取入参key失败~~~";
+        napi_create_string_utf8(env, msg, sizeof(msg), &napiResult);
+    }
+    // 获取待加密的字符串
+    char str[256];
+    size_t strLen;
+    napi_status stStr = napi_get_value_string_utf8(env, args[1], str, sizeof(str), &strLen);
+    if (stStr != napi_ok) {
+        char msg[] = "读取入参待加密字符串失败~~~";
+        napi_create_string_utf8(env, msg, sizeof(msg), &napiResult);
     }
 
+    napi_create_string_utf8(env, key, sizeof(key), &napiResult);
+//     napi_create_string_utf8(env, str, sizeof(str), &napiResult);
 
-    napi_value napiResult;
+    // 开始加密
+    //  将密钥字符串转换为DES_cblock
+    //     DES_cblock keyCblock;
+    //     memcpy(keyCblock, key, sizeof(key));
 
-    return napiResult;
-}
+    // 将数据字符串转换为DES_cblock
+    //     DES_cblock data;
+    //     memcpy(data, str, sizeof(str));
 
-static napi_value decodeCBC(napi_env env, napi_callback_info info) {
-    if ((nullptr == env) || (nullptr == info)) {
-        OH_LOG_Print(LOG_APP, LOG_ERROR, 0xFF00, "MyHypot", "env or exports is null");
-        return nullptr;
-    }
-
-    // Number of parameters.
-    size_t argc = 2;
-
-    // Declare parameter array.
-    napi_value args[2] = {nullptr};
-
-    // Gets the arguments passed in and puts them in the argument array.
-    if (napi_ok != napi_get_cb_info(env, info, &argc, args, nullptr, nullptr)) {
-        OH_LOG_Print(LOG_APP, LOG_ERROR, 0xFF00, "MyHypot", "api_get_cb_info failed");
-        return nullptr;
-    }
+    // 加密数据
+    //     DES_key_schedule sch;
+    //     DES_set_key_unchecked(&keyCblock, &sch);
+    //     DES_ecb_encrypt(&data, &data, &sch, DES_ENCRYPT);
 
 
-    napi_value napiResult;
+    //     napi_create_string_utf8(env, reinterpret_cast<const char *>(&data), sizeof(&data), &napiResult);
 
     return napiResult;
 }
 
-/**
- * Native C++模板生成的结构
- * @param env
- * @param exports
- * @return
- */
+
+static napi_value EncodeECB(napi_env env, napi_callback_info info) {
+    //
+    //     //     DES_ecb_encrypt(const_DES_cblock *input, DES_cblock *output, DES_key_schedule *ks, int enc)
+    //
+    //     // Number of parameters.
+    // //     size_t argc = 0;
+    //
+    //     // argv 传入 nullptr 来获取传入参数真实数量
+    // //     napi_get_cb_info(env, info, &argc, nullptr, nullptr, nullptr);
+    //     // JS 传入参数为0，不执行后续逻辑
+    // //     if (argc == 0) {
+    // //         return nullptr;
+    // //     }
+    //
+    //     // Declare parameter array.
+    //     // 创建数组用以获取JS传入的参数
+    // //     napi_value *argv = new napi_value[argc];
+    //
+    // //     napi_get_cb_info(env, info, &argc, argv, nullptr, nullptr);
+    //
+    // //     napi_valuetype strType;
+    // //     napi_typeof(env, argv[0], &strType);
+    //
+    // //     napi_valuetype keyType;
+    // //     napi_typeof(env, argv[1], &keyType);
+    //
+    napi_value napiResult;
+    //
+    //
+    napi_create_string_utf8(env, "您好这是返回的字符串", 100, &napiResult);
+    //
+    //     // argv 为 new 创建的对象，在使用完成后手动释放
+    //     //     delete argv;
+    //
+    return napiResult;
+}
+
 EXTERN_C_START
-static napi_value Init(napi_env env, napi_value exports)
-{
-    if ((nullptr == env) || (nullptr == exports)) {
-        OH_LOG_Print(LOG_APP, LOG_ERROR, 0xFF00, "Init", "env or exports is null");
-        return exports;
-    }
-    /**
-     * 需要将编写的自定义方法与对外提供的接口xxxxx接口进行关联
-     */
+static napi_value Init(napi_env env, napi_value exports) {
     napi_property_descriptor desc[] = {
-        {"encodeECB", nullptr, encodeECB, nullptr, nullptr, nullptr, napi_default, nullptr},
-        {"decodeECB", nullptr, decodeECB, nullptr, nullptr, nullptr, napi_default, nullptr},
-        {"encodeCBC", nullptr, encodeCBC, nullptr, nullptr, nullptr, napi_default, nullptr},
-        {"decodeCBC", nullptr, decodeCBC, nullptr, nullptr, nullptr, napi_default, nullptr}
+        {"add", nullptr, Add, nullptr, nullptr, nullptr, napi_default, nullptr},
+        {"encodeECB", nullptr, EncodeECB, nullptr, nullptr, nullptr, napi_default, nullptr},
+        {"encodeCBC", nullptr, EncodeCBC, nullptr, nullptr, nullptr, napi_default, nullptr},
     };
     napi_define_properties(env, exports, sizeof(desc) / sizeof(desc[0]), desc);
     return exports;
 }
 EXTERN_C_END
 
-/**
- * 配置模块描述信息，设置Init方法为napi_module的入口方法。
- */
 static napi_module demoModule = {
     .nm_version = 1,
     .nm_flags = 0,
     .nm_filename = nullptr,
     .nm_register_func = Init,
     .nm_modname = "efndk",
-    .nm_priv = ((void*)0),
-    .reserved = { 0 },
+    .nm_priv = ((void *)0),
+    .reserved = {0},
 };
 
-/**
- * 系统自动调用，使用NAPI接口napi_module_register()传入模块描述信息进行模块注册
- */
-extern "C" __attribute__((constructor)) void RegisterEfndkModule(void)
-{
-    napi_module_register(&demoModule);
-}
+extern "C" __attribute__((constructor)) void RegisterEfndkModule(void) { napi_module_register(&demoModule); }
--- efndk/src/main/cpp/types/libefndk/index.d.ts
+++ efndk/src/main/cpp/types/libefndk/index.d.ts
@@ -1,3 +1,5 @@
+export const add: (a: number, b: number) => number;
+
 /**
  * 加密-ECB模式
  * @author csx
@@ -5,33 +7,7 @@
  * @param key  des的key
  * @returns 加密后的字符串
  */
-export const encodeECB: (str: string, key: string) => string;
+export const encodeECB: (str: string, key: string, iv: string) => string;
 
-/**
- * 解密-ECB模式
- * @author csx
- * @param str 待解密的字符串
- * @param key  des的key
- * @returns 解密后的字符串
- */
-export const decodeECB: (str: string, key: string) => string;
 
-/**
- * 加密-CBC模式 需要传入iv偏移量字符串
- * @author csx
- * @param str 待加密的字符串
- * @param iv 偏移量
- * @param key  des的key
- * @returns 加密后的字符串
- */
-export const encodeCBC: (str: string, iv: string, key: string) => string;
-
-/**
- * 解密-CBC模式 需要传入iv偏移量字符串
- * @author csx
- * @param str 待解密的字符串
- * @param iv 偏移量
- * @param key  des的key
- * @returns 解密后的字符串
- */
-export const decodeCBC: (str: string, iv: string, key: string) => string;
+export const encodeCBC: (str: string, key: string) => string;
